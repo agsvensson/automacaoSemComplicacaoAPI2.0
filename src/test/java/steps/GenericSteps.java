279d01ec025f4fdb1b314a581e9260423a144d32
Arthur Svensson <guimaraesarthur86@yahoo.com.br>
2023-03-11 19:47:59 -0300

    Aula 14: Cucumber, Tags e DataTable

--- src/test/java/steps/GenericSteps.java
+++ src/test/java/steps/GenericSteps.java
@@ -1,17 +1,14 @@
 package steps;
 
 import io.cucumber.java.pt.Entao;
+import org.junit.Assert;
+import utils.RestUtils;
 
 public class GenericSteps {
     @Entao("valido que recebo status {int} no response")
-    public void validoQueReceboStatusNoResponse(Integer int1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Entao("armazeno o token que recebo do response de Login")
-    public void armazenoOTokenQueReceboDoResponseDeLogin() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void validoQueReceboStatusNoResponse(int status) {
+        Assert.assertEquals(status, RestUtils.getResponse().statusCode());
+
     }
 
 }
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,19 +1,40 @@
 package steps;
 
 import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
 import io.cucumber.java.pt.Quando;
+import io.restassured.http.ContentType;
+import maps.LoginMap;
+import utils.RestUtils;
+
+import java.util.Map;
 
 public class LoginSteps {
 
+    String url = "http://localhost:8080/";
+
     @Dado("que tenha um payload valido da API de Login")
     public void queTenhaUmPayloadValidoDaAPIDeLogin() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        LoginMap.initLogin();
+        RestUtils.setBaseURI(url);
     }
-    @Quando("envio uma requisição do tipo POST de Login")
-    public void envioUmaRequisiçãoDoTipoPOSTDeLogin() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    @Quando("envio uma requisicao do tipo POST de Login")
+    public void envioUmaRequisicaoDoTipoPOSTDeLogin() {
+        RestUtils.post(LoginMap.getLogin(), ContentType.JSON, "auth");
+    }
+
+    @Dado("que tenha um payload da API de login com as seguintes informacoes")
+    public void queTenhaUmPayloadDaAPIDeLoginComAsSeguintesInformacoes(Map<String, Object> map) {
+        LoginMap.initLogin();
+        RestUtils.setBaseURI(url);
+        LoginMap.getLogin().putAll(map);
+    }
+
+
+    @Entao("armazeno o token que recebo do response de Login")
+    public void armazenoOTokenQueReceboDoResponseDeLogin() {
+        LoginMap.token = RestUtils.getResponse().jsonPath().get("token");
     }
 
 }
