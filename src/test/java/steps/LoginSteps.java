a74c60e120861750dc977c6ab9b4319baf7de549
Arthur Svensson <guimaraesarthur86@yahoo.com.br>
2023-03-11 18:12:05 -0300

    Aula 13: Cucumber, Runner e pacotes

---
+++ src/test/java/steps/LoginSteps.java
@@ -0,0 +1,19 @@
+package steps;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Quando;
+
+public class LoginSteps {
+
+    @Dado("que tenha um payload valido da API de Login")
+    public void queTenhaUmPayloadValidoDaAPIDeLogin() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Quando("envio uma requisição do tipo POST de Login")
+    public void envioUmaRequisiçãoDoTipoPOSTDeLogin() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
